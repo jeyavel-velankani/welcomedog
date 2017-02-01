d6c9ac871c89ee2b63fb737439aa6812db680b9f
Jeyavel.Natesan <jeyavel.natesan@velankani.com>
2017-02-01 12:20:40 +0530

    add new gem

---
+++ lib/welcomedog.rb
@@ -0,0 +1,20 @@
+require "welcomedog/version"
+
+module Welcomedog
+  class Greeting
+	  def self.process(hour_of_clock)
+	   	greet(hour_of_clock)
+	  end
+	  def greet(hour_of_clock)
+			if hour_of_clock >= 6 && hour_of_clock <= 11
+		    "Good Morning"
+		  elsif hour_of_clock >= 12 && hour_of_clock <= 16
+		    "Good Afternoon"
+		  elsif hour_of_clock >= 17 && hour_of_clock <= 20
+		    "Good Evening"
+		  else
+		    "Good Night"
+		  end
+		end
+ end
+end
---
+++ lib/welcomedog/version.rb
@@ -0,0 +1,3 @@
+module Welcomedog
+  VERSION = "0.1.0"
+end
